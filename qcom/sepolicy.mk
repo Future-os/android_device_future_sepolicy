d060942269966da0a5970f7f25ddaa96d4b9c9c6
Sankar <sankarapspk@users.noreply.github.com>
2019-01-21 07:39:33 -0800

    init future

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,11 +1,11 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from Lineage
+# that inherit from Future
 #
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
+    device/future/sepolicy/qcom/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/common \
-    device/lineage/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/future/sepolicy/qcom/common \
+    device/future/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
